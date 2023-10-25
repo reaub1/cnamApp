22b86a13de6083cd404c864465b629e4b4af8af1
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2023-10-25 18:44:09 +0200

    controleur

---
+++ deploy/api/controller.php
@@ -0,0 +1,89 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+	function optionsCatalogue (Request $request, Response $response, $args) {
+	    
+	    // Evite que le front demande une confirmation à chaque modification
+	    $response = $response->withHeader("Access-Control-Max-Age", 600);
+	    
+	    return addHeaders ($response);
+	}
+
+	function  getSearchCalatogue (Request $request, Response $response, $args) {
+	    $filtre = $args['filtre'];
+	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
+	   
+	    if ($filtre) {
+	      $data = json_decode($flux, true); 
+	    	
+		$res = array_filter($data, function($obj) use ($filtre)
+		{ 
+		    return strpos($obj["titre"], $filtre) !== false;
+		});
+		$response->getBody()->write(json_encode(array_values($res)));
+	    } else {
+		 $response->getBody()->write($flux);
+	    }
+
+	    return $response;
+	}
+
+	// API Nécessitant un Jwt valide
+	function getCatalogue (Request $request, Response $response, $args) {
+	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
+	    $data = json_decode($flux, true); 
+	    
+	    $response->getBody()->write(json_encode($data));
+	    
+	    return $response;
+	}
+
+	function optionsUtilisateur (Request $request, Response $response, $args) {
+	    
+	    // Evite que le front demande une confirmation à chaque modification
+	    $response = $response->withHeader("Access-Control-Max-Age", 600);
+	    
+	    return addHeaders ($response);
+	}
+
+	// API Nécessitant un Jwt valide
+	function getUtilisateur (Request $request, Response $response, $args) {
+	    global $entityManager;
+	    
+	    $payload = getJWTToken($request);
+	    $login  = $payload->userid;
+	    
+		$data = array('nom' => 'Martin', 'prenom' => 'Jacques');
+		$response->getBody()->write(json_encode($data));
+
+	    return $response;
+	}
+
+	// APi d'authentification générant un JWT
+	function postLogin (Request $request, Response $response, $args) {   
+	    global $entityManager;
+	    $err=false;
+	    $body = $request->getParsedBody();
+	    $login = $body ['login'] ?? "";
+	    $pass = $body ['password'] ?? "";
+
+	    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$login))   {
+		$err = true;
+	    }
+	    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$pass))  {
+		$err=true;
+	    }
+		if (!$err) {
+			$response = createJwT ($response);
+			$data = array('nom' => 'Martin', 'prenom' => 'Jacques');
+			$response->getBody()->write(json_encode($data));
+		} else {          
+				$response = $response->withStatus(500);
+		}
+
+		return $response;
+	}
+
+
+
