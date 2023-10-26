8bce44176700d94a303c50d85438785afca4a681
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2023-10-26 13:53:20 +0200

    middleware

--- deploy/api/route.php
+++ deploy/api/route.php
@@ -23,3 +23,5 @@
 	// APi d'authentification générant un JWT
 	$app->post('/api/utilisateur/login', 'postLogin');
 
+	// APi d'authentification générant un JWT
+	$app->post('/api/login', 'postLogin');
