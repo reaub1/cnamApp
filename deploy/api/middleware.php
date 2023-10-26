d0f1d8c764f71f71ba4d5f418593e9b06b629302
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2023-10-26 13:55:55 +0200

    middleware

--- deploy/api/middleware.php
+++ deploy/api/middleware.php
@@ -37,7 +37,7 @@
 	    "algorithm" => ["HS256"],
 	    "secret" => JWT_SECRET,
 	    "path" => ["/api"],
-	    "ignore" => ["/api/hello","/api/utilisateur/login"],
+	    "ignore" => ["/api/hello","/api/utilisateur/login","/api/login"],
 	    "error" => function ($response, $arguments) {
 		$data = array('ERREUR' => 'Connexion', 'ERREUR' => 'JWT Non valide');
 		$response = $response->withStatus(401);
