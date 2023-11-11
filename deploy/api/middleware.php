e873269e435e883e92cffdec926110c7ce768d39
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2023-11-11 23:02:12 +0100

    expose header

--- deploy/api/middleware.php
+++ deploy/api/middleware.php
@@ -46,11 +46,12 @@
 
 	function  addHeaders (Response $response) : Response {
 	    $response = $response
-	    ->withHeader("Content-Type", "application/json");
-	    //->withHeader('Access-Control-Allow-Origin', ('https://tpbackend.herokuapp.com'))
+	    ->withHeader("Content-Type", "application/json")
+	    ->withHeader('Access-Control-Expose-Headers', 'Authorization');
+		//->withHeader('Access-Control-Allow-Origin', ('https://tpbackend.herokuapp.com'))
 	    //->withHeader('Access-Control-Allow-Headers', 'Content-Type,  Authorization')
 	    //->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-	    //->withHeader('Access-Control-Expose-Headers', 'Authorization');
+	    
 
 	    return $response;
 	}
