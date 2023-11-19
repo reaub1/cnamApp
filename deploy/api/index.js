4bb09021d1eeccd603a13e34f88edb468b0fb87b
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2023-11-19 17:59:19 +0100

    basic controller

--- deploy/api/index.js
+++ deploy/api/index.js
@@ -18,12 +18,6 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to CNAM application." });
-});
-
-
 require("./routes/utilisateur.routes")(app);
 
 // set port, listen for requests
