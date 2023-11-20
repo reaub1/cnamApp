ff362166e56bfddf36e20a5b074b894f8531de7e
emmamau <emmanuel.maurice@gmail.com>
2023-11-20 14:32:12 +0100

    Update index.js
    Signed-off-by: emmamau <emmanuel.maurice@gmail.com>

--- deploy/api/routes/index.js
+++ deploy/api/routes/index.js
@@ -1,3 +1,4 @@
-require("./catalogue.routes")(app);
-require("./utilisateur.routes")(app);
-
+module.exports = app => {  
+  require("./catalogue.routes")(app);
+  require("./utilisateur.routes")(app);
+}
