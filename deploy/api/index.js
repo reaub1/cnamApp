9c747bc869b96f58f225a8bc5a912ffc5d9edcb4
emmamau <emmanuel.maurice@gmail.com>
2023-11-20 10:48:56 +0100

    Update index.js
    Signed-off-by: emmamau <emmanuel.maurice@gmail.com>

--- deploy/api/index.js
+++ deploy/api/index.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+require("./routes/catalogue.routes")(app);   
+require("./routes/utilisateur.routes")(app);
 
-
-require("./routes")(app);
 // set port, listen for requests
 const PORT =  443;
 app.listen(PORT, () => {
