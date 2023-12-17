b90a1cb9e77e95fb86798a66baeacba7f8f207fe
emmamau <emmanuel.maurice@gmail.com>
2023-12-17 14:36:01 +0100

    Update bootstrap.php
    Signed-off-by: emmamau <emmanuel.maurice@gmail.com>

--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -6,14 +6,15 @@
 	$isDevMode = true;
 	$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 	$conn = array(
-	'host' => 'dpg-ck3ie7b6fquc73cq3a50-a.oregon-postgres.render.com',
+	'host' => 'dpg-clvf4ida73kc73bp3kfg-a.oregon-postgres.render.com',
 
 	'driver' => 'pdo_pgsql',
-	'user' => 'cnam_db_92ul_user',
-	'password' => 'Z0OjNnVAavrSDdSI6biVgys2U3N52Z3Q',
-	'dbname' => 'cnam_db_92ul',
+	'user' => 'cnam_db_akkt_user',
+	'password' => 'I8TW2jVgiSyQDguxH8De6kWjgJ6VjoR4',
+	'dbname' => 'cnam_db_akkt',
 	'port' => '5432'
 	);
+
 
 	$entityManager = EntityManager::create($conn, $config);
 
